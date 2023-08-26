ba967916df41ebd7ef0c2d0304cdd0447cf59f5b
McK2007 <2007chan0221@gmail.com>
2023-08-26 09:46:47 +0900

    list, for문 기본형태 학습

--- pythonBasic/ifStatement.py
+++ pythonBasic/ifStatement.py
@@ -1,6 +1,7 @@
 score = float(input('이번 시험의 점수를 입력하시오\n'))
-
-if score >= 90 :
+if score >100 or score < 0:
+    print('값을 잘못 입력하셨습니다')
+elif score >= 90 :
     print('A반입니다')
 elif score >= 80 :
     print('B반입니다')
