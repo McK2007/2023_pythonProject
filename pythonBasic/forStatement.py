2aa8493f73039c19409613d5369cdb128f83c2d2
McK2007 <2007chan0221@gmail.com>
2023-08-26 10:47:08 +0900

    ifStatement

--- pythonBasic/forStatement.py
+++ pythonBasic/forStatement.py
@@ -7,7 +7,7 @@ print(numbers[0])
 print(strings[0])
 print(anys[3][2])
 
-scores = [84, 65, 79, 91, 50, 80, 54, 23, 58, 98, 64, 95, 90,39, 84, 67, 82, 99, 91]
+scores = [84, -5, 65, 79, 105, 100, 91, 50, -32, 80, 54, 23, 58, 98, 64, 95, 90, 39, 84, 67, 82, 99, 91]
 
 print(scores[3:])
 print(scores[1:4])
@@ -15,6 +15,9 @@ print(scores[:3])
 
 x = 0
 for i in scores:
+    if i > 100 or i <0:
+        print('skip' , i)
+        continue
     x += i
     print(i, x)
 x /= len(scores)
