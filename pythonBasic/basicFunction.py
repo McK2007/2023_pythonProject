c8ddf8c3a204627739dce0dcbe6d9e57d15d78b1
McK2007 <2007chan0221@gmail.com>
2023-09-23 10:21:13 +0900

    함수 정의하기

---
+++ pythonBasic/basicFunction.py
@@ -0,0 +1,68 @@
+# def tmpFunction(x):
+#     return 3*x +5
+# x = int(input())
+# print(tmpFunction(x))
+
+# 게임
+
+def menuPrint():
+    print('========GAME========')
+    print('1.행맨')
+    print('2.업다운')
+    print('0.종료')
+    print('====================')
+
+def getRandomWord():
+    import random
+    words = ['hang', 'pretty', 'apple', 'ant', 'water', 'samsung', 'MCdonalds', 'fluent', 'voca', 'galaxy']
+    return words[random.randrange(0, len(words))]
+
+hangman_input_history = []
+
+def getHangmanInput():
+    while True:
+        user_input = input("Input alphabet ::: ")
+        if(user_input.isalpha()): #알파벳인지 확인
+            alphabet = user_input[0].lower()
+            if(hangman_input_history.index(alphabet)): #이미 입력된 값인지 확인
+                print("이미 입력한 값입니다. 새로운 알파벳을 입력해주세요.")
+            else:
+                return alphabet
+def runHangMan():
+    hangman_input_history = []
+    word = getRandomWord()
+    print('_'*len(word))
+    alpahbet = getHangmanInput()
+
+def runUpDown():
+    import random
+
+    answer = random.randrange(0, 10)
+    chance = 3
+
+    while chance > 0:
+        user_input = int(input())
+        if answer == user_input:
+            print('correct')
+            break
+        else:
+            chance -= 1
+            if chance > 0:
+                print('chance:', chance)
+                print('try again')
+                if user_input < answer:
+                    print('up')
+                else:
+                    print('down')
+            else:
+                print('game_over')
+
+
+user_Input = -1
+while user_Input != 0:
+    menuPrint()
+    user_Input = int(input('select menu\n'))
+    if user_input == 1:
+        runHangMan()
+    elif user_Input == 2:
+        runUpDown()
--- pythonBasic/whileStatement.py
+++ pythonBasic/whileStatement.py
@@ -21,6 +21,7 @@ user_input = -1
 # 사용자가 answer 맞출때까지 반복
 # 1. 사용자에게 기회주기
 # 2. 틀렸을때 updown 출력해주기
+chance = 3
 
 while True:
     user_input = int(input())
@@ -28,6 +29,11 @@ while True:
         print('correct')
         break
     else:
+        chance -= 1
+        if chance == 0:
+            print('game_over')
+            break
+        print('chance:', chance)
         print('try again')
         if user_input < answer:
             print('up')
